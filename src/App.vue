4d6d6a0b95dd02a4bc2cadb0b52120ffb83e684a
coderwhy <coderwhy@gmail.com>
2022-10-16 08:55:06 +0800

    cms_day02

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,35 @@
 <template>
   <div class="app">
-    <h2>App</h2>
-    <router-link to="/main">主要</router-link>
-    <router-link to="/login">登录</router-link>
     <router-view></router-view>
+
+    <!-- <el-row class="mb-4">
+      <el-button>Default</el-button>
+      <el-button type="primary">Primary</el-button>
+      <el-button type="success">Success</el-button>
+      <el-button type="info">Info</el-button>
+      <el-button type="warning">Warning</el-button>
+      <el-button type="danger">Danger</el-button>
+    </el-row>
+    <div>
+      <el-link href="https://element.eleme.io" target="_blank">default</el-link>
+      <el-link type="primary">primary</el-link>
+      <el-link type="success">success</el-link>
+      <el-link type="warning">warning</el-link>
+      <el-link type="danger">danger</el-link>
+      <el-link type="info">info</el-link>
+    </div> -->
+
+    <!-- <el-slider v-model="value" show-input size="large" />
+    <el-slider v-model="value" show-input />
+    <el-slider v-model="value" show-input size="small" /> -->
   </div>
 </template>
 
-<script setup lang="ts">
-interface IKun {
-  name: string
-}
+<script setup lang="ts"></script>
 
-const ikun: IKun = {
-  name: 'kobe'
+<style scoped>
+.app {
+  width: 100vw;
+  height: 100vh;
 }
-console.log(ikun)
-</script>
-
-<style scoped></style>
+</style>
