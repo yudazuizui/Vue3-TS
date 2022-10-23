e7bfd8fff882e830d3e21c17aaabbad1dfb03cd7
coderwhy <coderwhy@gmail.com>
2022-10-23 17:36:48 +0800

    修复fix

--- src/main.ts
+++ src/main.ts
@@ -3,20 +3,11 @@ import 'normalize.css'
 import './assets/css/index.less'
 import App from './App.vue'
 import router from './router'
-import store from './store';
-import icons from './global/register-icons';
-
-
-
-
-
-
+import store from './store'
+import icons from './global/register-icons'
 
 const app = createApp(App)
 app.use(icons)
-
-
-
 
 app.use(store)
 app.use(router)
